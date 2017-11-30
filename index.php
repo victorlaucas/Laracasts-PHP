6dcec421a6ffaff0e2a3061b6d35b3b5d35adc15
Victor <victorlaucas@gmail.com>
2017-11-29 23:06:37 -0700

    Learned about separation of concerns

--- index.php
+++ index.php
@@ -1,28 +1,5 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="UTF-8">
-    <title>title</title>
+<?php 
 
-    <style type="text/css">
-      header {
-        background-color: #e3e3e3;
-        padding: 2em;
-        text-align: center;
-      }
-    </style>
-  </head>
-  <body>
-    
-    <header>
-      <h1>
-        <?php 
-          $name = $_GET['name'];
+$greeting = "Hello World";
 
-          echo "Hello, $name"
-        ?>
-      </h1>
-    </header>
-  
-  </body>
-</html>
+require 'index.view.php';
---
+++ index.view.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>title</title>
+
+    <style type="text/css">
+      header {
+        background-color: #e3e3e3;
+        padding: 2em;
+        text-align: center;
+      }
+    </style>
+  </head>
+  <body>
+    
+    <header>
+      <h1>
+        <?= $greeting; ?>
+      </h1>
+    </header>
+  
+  </body>
+</html>
