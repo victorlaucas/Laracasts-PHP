725c58c586652ed83a003338d93898428a266171
Victor <victorlaucas@gmail.com>
2017-12-04 09:29:56 -0700

    First half of php classes video

--- index.php
+++ index.php
@@ -1,34 +1,33 @@
 <?php 
 
-class Task {
+require 'function.php';
 
+class Task {
   protected $description;
 
   protected $completed = false;
 
-  public function __contruct($description)
-
-  {
-    // Auto triggered on instantiation
+  public function __construct($description){
+    // Auto triggered on instantiation 
     $this->description = $description;
   }
 
-  public function complete()
-  {
-    $this->completed = true;
+  public function isComplete(){
+    return $this->completed;
   }
 
-  public function isComplete()
-  
-  {
-    return $this->completed;
+  public function complete(){
+    $this->completed = true;
   }
 }
 
 $task = new Task('Go to the store');
 
-$task-> complete;
+$task->complete();
 
 var_dump($task->isComplete());
+
+
+
 
 // require 'index.view.php';
