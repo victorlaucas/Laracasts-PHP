0dc4ed2ca5b880e4a6dc0093a24eec8e2b258c38
Victor <victorlaucas@gmail.com>
2017-12-04 14:12:22 -0700

    Intro to PDOs, refactoring same code

--- function.php
+++ function.php
@@ -1,5 +1,24 @@
 <?php
 
+function conntectToDb(){
+  try {
+    return new PDO('mysql:host=127.0.0.1;dbname=mytodo', 'root', '');  
+  } catch (PDOExeption $e) {
+      die($e->getMessage());
+  }
+}
+
+function fetchAllTasks($pdo){
+  // Prepare sql query 
+  $statement = $pdo->prepare('select * from todos');
+
+  // Execute sql query
+  $statement->execute();
+
+  // Fetch all results of this query 
+  return $statement->fetchAll(PDO::FETCH_CLASS, 'Task');  
+}
+
 function dd($data) {
   echo '<pre>';
   die(var_dump($data));
--- index.php
+++ index.php
@@ -1,23 +1,11 @@
 <?php 
 
+require 'function.php';
+
 require 'Task.php';
 
-// Try to connect to database
-try {
-  $pdo = new PDO('mysql:host=127.0.0.1;dbname=mytodo', 'root', '');  
-} catch (PDOExeption $e) {
-  die($e->getMessage());
-}
+$pdo = conntectToDb();
 
-// Prepare sql query 
-$statement = $pdo->prepare('select * from todos');
-
-// Execute sql query
-$statement->execute();
-
-// Fetch all results of this query 
-$tasks = $statement->fetchAll(PDO::FETCH_CLASS, 'Task');
-
-
+$tasks = fetchAllTasks($pdo);
 
 require 'index.view.php';
