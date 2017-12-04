eccce01045934dd4445cd41a7276b3aa78e3861c
Victor <victorlaucas@gmail.com>
2017-12-03 22:44:16 -0700

    Laracast commit

--- index.view.php
+++ index.view.php
@@ -26,30 +26,6 @@
       <li>
         <strong>Person Responsible: </strong> <?= $task['assigned_to']; ?>
       </li>
-
-      <li>
-
-        <strong>Status: </strong>
-        <?php
-
-        if ($task['completed']) {
-          echo '&#9989;';
-        } else {
-          echo "Incomplete";
-        }
-
-        ?>
-
-        <!-- Short hand for if -->
-
-        <strong>Status: </strong>
-
-        <?php if ($task['completed']) : ?>
-          <span class="icon">&#9989;</span>
-        <?php else : ?>
-          <span class="icon">Incomplete</span>
-        <?php endif; ?>
-      </li>
     </ul>
   </body>
 </html>
